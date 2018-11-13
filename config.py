73e953f60538fe33ed873e170a181f87b4774294
abghorba <andrewbghorbani@gmail.com>
2018-11-12 16:37:20 -0800

    AWS deployment

---
+++ config.py
@@ -0,0 +1,8 @@
+class SecretKey:
+    SECRET_KEY = '9\xa4)\xce\x0c\xc6\x9a\x7fi:*}\xd9\x9d\xd8\xe8\xba\x0b\xa3\xcb6X4\x8e'
+
+class MySQL_DB:
+    DB_HOST = 'myfinance-db.cioldyovwirn.us-east-2.rds.amazonaws.com'
+    DB_USER = 'abghorba'
+    DB_PASSWORD = '1a2b3c4d$$'
+    DB_NAME = 'finance'
